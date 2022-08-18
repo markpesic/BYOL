46c2d574af87dd7989ed448a958158822b400580
Marco Pesic <markvpe@gmail.com>
2022-08-18 22:07:17 +0200

    some fixes and eliminaing useless code

--- BYOL/utils.py
+++ BYOL/utils.py
@@ -10,25 +10,6 @@ def regression_loss(x, y):
 def criterion(xOn, yTg, yOn, xTg):
     return (regression_loss(xOn, yTg) + regression_loss(yOn, xTg)).mean()
 
-transformT = tr.Compose([
-    tr.RandomResizedCrop(size=32, scale=(0.08,1), ratio=(3 / 4, 4 / 3)),
-    tr.RandomApply(nn.ModuleList([tr.RandomRotation((-90, 90))]), p=0.5),
-    tr.RandomApply(nn.ModuleList([tr.ColorJitter()]), p=0.8),
-    tr.GaussianBlur(kernel_size=(23,23), sigma=(0.1, 2.0)),
-    #tr.RandomGrayscale(p=0.2),
-    tr.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))])
-
-transformT1 = tr.Compose([
-    tr.RandomResizedCrop(size=32, scale=(0.08,1), ratio=(3 / 4, 4 / 3)),
-    tr.RandomApply(nn.ModuleList([tr.RandomRotation((-90, 90))]), p=0.5),
-    tr.RandomApply(nn.ModuleList([tr.ColorJitter()]), p=0.8),
-    #tr.RandomGrayscale(p=0.2),
-    tr.RandomApply(nn.ModuleList([tr.GaussianBlur(kernel_size=(23,23), sigma=(0.1, 2.0))]), p=0.1),
-    tr.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))])
-
-transformEvalT = tr.Compose([
-    tr.CenterCrop(size=(32,32))
-])
 
 def get_byol_transforms(size, mean, std):
     transformT = tr.Compose([
